ea9e14b3cfe6d207803adf5c57e1a08d22923f66
Stephen Newkirk <newkirk771@gmail.com>
2016-08-19 09:08:21 -0700

    Make UI js logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 //==BUSINESS=LOGIC==
 
+
 //==UI=LOGIC==
+$(document).ready(function() {
+  $("form").submit(function(event){
+    event.preventDefault();
+    var userInput = parseInt($("input#numberInput").val());
+    pingPong(userInput);
+  });
+});
