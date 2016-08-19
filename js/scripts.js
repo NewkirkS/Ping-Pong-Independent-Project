84472d8c8b7bb28c158c7accecbdaf14f1479d17
Stephen Newkirk <newkirk771@gmail.com>
2016-08-19 09:42:28 -0700

    Flesh out ui logic to display results. Create basic back-end loop to generate number list. First behavior accomplished. Bug with undefined showing up in list on multiple submissions without page reload.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 //==BUSINESS=LOGIC==
-
+var numbers = [];
+var indexCounter = 0;
+var pingPong = function(number) {
+  for(i=1; i <= number; i++) {
+    numbers.push(i);
+  }
+}
 
 //==UI=LOGIC==
 $(document).ready(function() {
@@ -7,5 +13,10 @@ $(document).ready(function() {
     event.preventDefault();
     var userInput = parseInt($("input#numberInput").val());
     pingPong(userInput);
+    numbers.forEach(function() {
+      $("#outputUl").append("<li>" + numbers[indexCounter] + "</li>");
+      indexCounter++;
+    });
+    numbers.length = 0;
   });
 });
